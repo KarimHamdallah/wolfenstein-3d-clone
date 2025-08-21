feb7731cee8689eae76586519486bd6ddf60a507
KarimHamdallah <108572488+KarimHamdallah@users.noreply.github.com>
2025-08-22 01:38:43 +0300

    - add ray casting

--- wolfenstein-3d-clone/src/main.cpp
+++ wolfenstein-3d-clone/src/main.cpp
@@ -1,4 +1,4 @@
-#include <iostream>
+﻿#include <iostream>
 #include <SDL3/SDL.h>
 #include <imgui.h>
 #include <backends/imgui_impl_sdl3.h>
@@ -135,6 +135,175 @@ Player player;
 
 /////////////////////////////////////////////////////////
 
+struct IntersectionData
+{
+	bool hit;
+	float x, y;
+};
+
+IntersectionData RayToLineIntersection(
+	float rx, float ry, float rdx, float rdy,
+	float x1, float y1, float x2, float y2)
+{
+	IntersectionData result{ false, 0.0f, 0.0f };
+
+	float sdx = x2 - x1;
+	float sdy = y2 - y1;
+
+	float denom = rdx * sdy - rdy * sdx;
+
+	if (fabs(denom) < 1e-6f)
+	{
+		return result;
+	}
+
+	float dx = x1 - rx;
+	float dy = y1 - ry;
+
+	float t = (dx * sdy - dy * sdx) / denom;
+
+	if (t >= 0.0f) 
+	{
+		result.hit = true;
+		result.x = rx + t * rdx;
+		result.y = ry + t * rdy;
+	}
+
+	return result;
+}
+
+
+
+inline float Distance(float x1, float y1, float x2, float y2)
+{
+	float dx = x2 - x1;
+	float dy = y2 - y1;
+	return std::sqrt(dx * dx + dy * dy);
+}
+
+inline float NormalizeAngle(float angle)
+{
+	angle = fmodf(angle, 2.0f * PI);   // wrap within [-2π, 2π]
+	if (angle < 0)
+		angle += 2.0f * PI;            // shift to [0, 2π)
+	return angle;
+}
+
+//////////////////// Ray ////////////////////////////////
+struct Ray
+{
+	float x, y;
+	float rotation_angle = PI / 2.0f;
+
+	bool isRayFacingDown = 0;
+	bool isRayFacingUp = 0;
+	bool isRayFacingRight = 0;
+	bool isRayFacingLeft = 0;
+
+	float min_intersection_dist = INFINITY;
+	float intersection_x = 0.0f;
+	float intersection_y = 0.0f;
+
+	void Cast()
+	{
+		min_intersection_dist = INFINITY;
+
+		float normalized_angle = NormalizeAngle(rotation_angle);
+		isRayFacingDown = normalized_angle > 0 && normalized_angle < PI;
+		isRayFacingUp = !isRayFacingDown;
+
+		isRayFacingRight = normalized_angle < 0.5 * PI || normalized_angle > 1.5 * PI;
+		isRayFacingLeft = !isRayFacingRight;
+
+		// horizontal intersections
+		for (size_t i = 0; i < TILE_ROW_NUM; i++)
+		{
+			auto hit = RayToLineIntersection(
+				x, y,
+				cosf(rotation_angle),
+				sinf(rotation_angle),
+				0.0f, TILE_SIZE * i, WINDOW_WIDTH, TILE_SIZE * i);
+
+			if (hit.hit)
+			{
+				int col = floor(hit.x / TILE_SIZE);
+				int raw = i;
+
+				if (isRayFacingUp)
+					raw = i - 1;
+
+				if (raw >= 0 && col >= 0 && raw < TILE_ROW_NUM && col < TILES_COL_NUM)
+				{
+					if (map[raw][col] != 0)
+					{
+						float dist = Distance(x, y, hit.x, hit.y);
+						if (min_intersection_dist > dist)
+						{
+							min_intersection_dist = dist;
+							intersection_x = hit.x;
+							intersection_y = hit.y;
+						}
+					}
+				}
+			}
+		}
+
+		// vertical intersections
+		for (size_t i = 0; i < TILES_COL_NUM; i++)
+		{
+			auto hit = RayToLineIntersection(
+				x, y,
+				cosf(rotation_angle),
+				sinf(rotation_angle),
+				TILE_SIZE * i, 0.0f, TILE_SIZE * i, WINDOW_HEIGHT);
+
+			if (hit.hit)
+			{
+				int raw = floor(hit.y / TILE_SIZE);
+				int col = i;
+
+				if (isRayFacingLeft)
+					col = i - 1;
+
+				if (raw >= 0 && col >= 0 && raw < TILE_ROW_NUM && col < TILES_COL_NUM)
+				{
+					if (map[raw][col] != 0)
+					{
+						float dist = Distance(x, y, hit.x, hit.y);
+						if (min_intersection_dist > dist)
+						{
+							min_intersection_dist = dist;
+							intersection_x = hit.x;
+							intersection_y = hit.y;
+						}
+					}
+				}
+			}
+		}
+	}
+
+	void Render(SDL_Renderer* renderer)
+	{
+		DrawOutlinedRect(renderer,
+			intersection_x,
+			intersection_y,
+			10.0f, 10.0f,
+			BLUE_COLOR, BLUE_COLOR);
+
+		SDL_SetRenderDrawColor(renderer, 0, 0, 255, 255);
+
+		SDL_RenderLine(renderer,
+			MAP_SCALING_FACTOR * (player.x + 0.5f * player.size),
+			MAP_SCALING_FACTOR * (player.y + 0.5f * player.size),
+			MAP_SCALING_FACTOR * (intersection_x),
+			MAP_SCALING_FACTOR * (intersection_y));
+	}
+};
+Ray ray;
+/////////////////////////////////////////////////////////
+
+
+
 uint64_t lastTime = SDL_GetTicks();
 float deltaTime = 0.0f;
 
@@ -172,6 +341,18 @@ int main(int argc, char** argv)
 
 	// alpha blending
 	SDL_SetRenderDrawBlendMode(renderer, SDL_BLENDMODE_BLEND);
+
+	// Setup Dear ImGui context
+	IMGUI_CHECKVERSION();
+	ImGui::CreateContext();
+	ImGuiIO& io = ImGui::GetIO(); (void)io;
+
+	// Setup Dear ImGui style
+	ImGui::StyleColorsDark();
+
+	// Setup Platform/Renderer bindings
+	ImGui_ImplSDL3_InitForSDLRenderer(window, renderer);
+	ImGui_ImplSDLRenderer3_Init(renderer);
 
 	// game loop
 
@@ -229,12 +410,18 @@ int main(int argc, char** argv)
 			default:
 				break;
 			}
+
+			ImGui_ImplSDL3_ProcessEvent(&event);
 		}
 
 		// update
 		player.Update(deltaTime);
 
 		// render
+
+		ImGui_ImplSDLRenderer3_NewFrame();
+		ImGui_ImplSDL3_NewFrame();
+		ImGui::NewFrame();
 
 		SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);  // black
 		SDL_RenderClear(renderer);
@@ -257,6 +444,12 @@ int main(int argc, char** argv)
 		// draw player
 		player.Render(renderer);
 
+		ray.x = player.x; ray.y = player.y; ray.rotation_angle = player.rotation_angle;
+		ray.Cast();
+		ray.Render(renderer);
+
+		ImGui::Render();
+		ImGui_ImplSDLRenderer3_RenderDrawData(ImGui::GetDrawData(), renderer);
 		SDL_RenderPresent(renderer);
 	}
 
